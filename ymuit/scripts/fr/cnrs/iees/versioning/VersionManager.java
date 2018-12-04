a37815513aedc62d351b895a0cba120cea53d446
Ian Davies <u9007333@anu.edu.au>
2018-12-04 12:01:54 +1100

    dep uit

--- ymuit/scripts/fr/cnrs/iees/versioning/VersionManager.java
+++ ymuit/scripts/fr/cnrs/iees/versioning/VersionManager.java
@@ -108,6 +108,7 @@ public class VersionManager {
 	 * 
 	 */
 	private static String[][] DEPS = { 
+			{"fr.cnrs.iees", "uit", "+"}
 	};
 	// ============================================================================================
 	
