941abc52832f7537ebb34b03c22d6bb58ecac686
Ian Davies <ian.davies@anu.edu.au>
2020-03-03 11:43:51 +1100

    comment

--- ymuit/src/au/edu/anu/ymuit/exceptions/YmuitException.java
+++ ymuit/src/au/edu/anu/ymuit/exceptions/YmuitException.java
@@ -1,29 +1,29 @@
 /**************************************************************************
- *  TW-CORE - 3Worlds Core classes and methods                            *
+ *  YMUIT - Yet More User-Interface Tools                                 *
  *                                                                        *
- *  Copyright 2018: Shayne Flint, Jacques Gignoux & Ian D. Davies         *
- *       shayne.flint@anu.edu.au                                          * 
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
  *       jacques.gignoux@upmc.fr                                          *
  *       ian.davies@anu.edu.au                                            * 
  *                                                                        *
- *  TW-CORE is a library of the principle components required by 3W       *
+ *  YMUIT contains a number of classes and helper methods for working     *
+ *  with java fx classes.                                                 *
  *                                                                        *
  **************************************************************************                                       
- *  This file is part of TW-CORE (3Worlds Core).                          *
+ *  This file is part of  YMUIT (Yet More User-Interface Tools).          *
  *                                                                        *
- *  TW-CORE is free software: you can redistribute it and/or modify       *
+ *  YMUIT is free software: you can redistribute it and/or modify         *
  *  it under the terms of the GNU General Public License as published by  *
  *  the Free Software Foundation, either version 3 of the License, or     *
  *  (at your option) any later version.                                   *
  *                                                                        *
- *  TW-CORE is distributed in the hope that it will be useful,            *
+ *  YMUIT is distributed in the hope that it will be useful,              *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *  GNU General Public License for more details.                          *                         
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
- *  along with TW-CORE.                                                   *
- *  If not, see <https://www.gnu.org/licenses/gpl.html>                   *
+ *  along with YMUIT.                                                     *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
  *                                                                        *
  **************************************************************************/
 package au.edu.anu.ymuit.exceptions;
