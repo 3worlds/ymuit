d03c22c2a0376e158e1f15844d192ccf8ff5c984
Jacques Gignoux <jacques.gignoux@upmc.fr>
2020-07-01 16:54:05 +0200

    code + version cleanup

--- ymuit/src/au/edu/anu/ymuit/util/Decimals.java
+++ ymuit/src/au/edu/anu/ymuit/util/Decimals.java
@@ -1,3 +1,32 @@
+/**************************************************************************
+ *  YMUIT - Yet More User-Interface Tools                                 *
+ *                                                                        *
+ *  Copyright 2018: Jacques Gignoux & Ian D. Davies                       *
+ *       jacques.gignoux@upmc.fr                                          *
+ *       ian.davies@anu.edu.au                                            * 
+ *                                                                        *
+ *  YMUIT contains a number of classes and helper methods for working     *
+ *  with java fx classes.                                                 *
+ *                                                                        *
+ **************************************************************************                                       
+ *  This file is part of  YMUIT (Yet More User-Interface Tools).          *
+ *                                                                        *
+ *  YMUIT is free software: you can redistribute it and/or modify         *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                   *
+ *                                                                        *
+ *  YMUIT is distributed in the hope that it will be useful,              *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *  GNU General Public License for more details.                          *                         
+ *                                                                        *
+ *  You should have received a copy of the GNU General Public License     *
+ *  along with YMUIT.                                                     *
+ *  If not, see <https://www.gnu.org/licenses/gpl.html>.                  *
+ *                                                                        *
+ **************************************************************************/
+
 package au.edu.anu.ymuit.util;
 
 import java.text.DecimalFormat;
