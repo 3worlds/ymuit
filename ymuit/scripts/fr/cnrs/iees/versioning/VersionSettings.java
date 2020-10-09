b5d3b8b1ad96be0c53935cc7b54c35afa7bee3db
Ian Davies <ian.davies@anu.edu.au>
2020-10-09 14:16:23 +1100

    correct dependency on generics 0.2.0

--- ymuit/scripts/fr/cnrs/iees/versioning/VersionSettings.java
+++ ymuit/scripts/fr/cnrs/iees/versioning/VersionSettings.java
@@ -83,7 +83,7 @@ public class VersionSettings {
 	 *
 	 */
 	protected static String[][] DEPS = {
-			{"fr.ens.biologie", "generics", "[0.1.17,)", null},
+			{"fr.ens.biologie", "generics", "[0.2.0,)", null},
 			{"fr.cnrs.iees.uit", "uit", "[0.2.4,)", null},
 			{"org.apache.commons" ,"commons-math","[2,)", null},
 //			{"org.apache.commons","commons-math3","[3.6.1,)",null},
